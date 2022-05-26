6dd33a139bddf06341d29222201f256b5e80c8c9
Satya <100060852+CrypticFate5@users.noreply.github.com>
2022-05-26 21:39:40 +0530

    Update Array Implementation.c

--- Array Implementation.c
+++ Array Implementation.c
@@ -1,84 +1,96 @@
 #include <stdio.h>
-int n=0;
-void insert(int a[],int p,int e){
-    for(int i=n-1;i>=p;i--){
-        a[i+1]=a[i];
+int n = 0;
+void insert(int a[], int p, int e)
+{
+    for (int i = n - 1; i >= p; i--)
+    {
+        a[i + 1] = a[i];
     }
-    a[p]=e;
-    n+=1;
+    a[p] = e;
+    n += 1;
     printf("Element successfully inserted!\n");
 }
-void delete(int a[],int p){
-    for(int i=p;i<n-1;i++){
-        a[i]=a[i+1];
+void delete (int a[], int p)
+{
+    for (int i = p; i < n - 1; i++)
+    {
+        a[i] = a[i + 1];
     }
-    n-=1;
+    n -= 1;
     printf("Element successfully deleted\n");
 }
-void search(int a[],int e){
-    int flag=0,i;
-    for(i=0;i<n;i++){
-        if(a[i]==e){
-            flag=1;
+void search(int a[], int e)
+{
+    int flag = 0, i;
+    for (i = 0; i < n; i++)
+    {
+        if (a[i] == e)
+        {
+            flag = 1;
             break;
         }
     }
-    if(flag==1){
-        printf("%d found at %d\n",e,i);
+    if (flag == 1)
+    {
+        printf("%d found at %d\n", e, i);
     }
-    else{
+    else
+    {
         printf("Element not found\n");
     }
 }
-void display(int a[]){
+void display(int a[])
+{
     printf("The elements are:\n");
-    for(int i=0;i<n;i++){
-        printf("%d ",a[i]);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
     }
     printf("\n");
 }
-int main(){
+int main()
+{
     int a[100];
     int c;
     printf("WELCOME TO ARRAY IMPLEMENTATION MENU\n");
-    do{
+    do
+    {
         printf("CHOOSE YOUR CHOICE\n");
         printf("1. Insert\n");
         printf("2. Delete\n");
         printf("3. Search\n");
         printf("4. Display\n");
         printf("5. Exit\n");
-        scanf("%d",&c);
-        int p,e;
-        switch(c){
-            case 1:
+        scanf("%d", &c);
+        int p, e;
+        switch (c)
+        {
+        case 1:
             printf("Enter the position and the element to be inserted: \n");
-            scanf("%d %d",&p,&e);
-            insert(a,p,e);
+            scanf("%d %d", &p, &e);
+            insert(a, p, e);
             display(a);
             break;
-            case 2:
+        case 2:
             printf("Enter the position to be deleted: \n");
-            scanf("%d",&p);
-            delete(a,p);
+            scanf("%d", &p);
+            delete (a, p);
             display(a);
             break;
-            case 3:
+        case 3:
             printf("Enter the element to be found: \n");
-            scanf("%d",&e);
-            search(a,e);
+            scanf("%d", &e);
+            search(a, e);
             break;
-            case 4:
+        case 4:
             display(a);
             break;
-            case 5:
+        case 5:
             break;
-            default:
+        default:
             printf("Invalid Choice!!\n");
             break;
         }
-    }
-    while(c<5);
+    } while (c < 5);
     return 0;
 }
-
